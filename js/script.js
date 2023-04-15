0791c3bbf68f471d0d31612b51df5ce606947a04
fatmaashraf1 <fatmaserry16@gmail.com>
2023-04-15 05:21:23 +0200

    Edit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
-//Add Student JS
-
-const add_student_button = document.querySelector("#add-student");
+// IMPORTANT!! LocalStorage Data
 const data = localStorage;
 var students=[];
 
+const open_button = document.querySelector("#popup");
+open_button.onclick = function showStatus(id){
+    students = JSON.parse(data.getItem("students"));
+
+    var target_student = students.filter( (element) =>
+        ( element.collageID == id )
+    )
+
+    document.getElementById("student_name_popup").innerHTML = target_student.first_name +" " + target_student.last_name;
+    document.getElementById("student_id").innerHTML = target_student.collageID;
+    document.getElementById("student_status_popup").innerHTML = target_student.status;
+}
+
+const save_button = document.querySelector("#save-status")
+save_button.onclick = (e) =>{
+    e.preventDefault();
+
+    target_student.status = document.getElementById("student-status-popup").value;
+
+    
+    students.push(target_student);
+    data.setItem("students",JSON.stringify)
+    
+}
+
+
+
+
+
+
+
+
+
+//Add Student JS
 
 
 class Student{
@@ -23,9 +55,12 @@ class Student{
         this.department = dep;
         this.date_of_birth = dob;
         this.gender = gender;
+        this.status = "Active";
     }
 }
 
+
+const add_student_button = document.querySelector("#add-student");
 add_student_button.onclick = (e) =>{
     e.preventDefault();
 
@@ -44,15 +79,19 @@ add_student_button.onclick = (e) =>{
     const date_of_birth = document.getElementById("sDOB").value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
 
-
+    // Validate Code
 
     const new_student = new Student(first_name,last_name,address,id,nationality,nationalityID,
             phone,landline_num,email,level,gpa,dep,date_of_birth,gender);
 
 
+    // push new_student to the main array students
     students.push(new_student);
+
+    // from array to string
     const s = JSON.stringify(students);
 
-    
+    // push the array as string to LocalStorage
     data.setItem("students",s);
+
 }
