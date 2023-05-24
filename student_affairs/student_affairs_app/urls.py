42f0784f82cc8a4e9f49591a319284f45d03d61e
belal <bebobale2021@gmail.com>
2023-05-24 20:57:52 +0300

    editStudent

--- student_affairs/student_affairs_app/urls.py
+++ student_affairs/student_affairs_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('view', views.view, name='view'),
-    path('search', views.search, name='search'),
+    path('index/search/', views.search, name='search'),
     path('edit_student', views.edit_student, name='edit_student')
 ]
