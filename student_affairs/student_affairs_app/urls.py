f129660510c07639da0c18185af04dade9e79072
Fatma Elzahraa Ashraf <fatmaserry16@gmail.com>
2023-05-24 21:00:03 +0300

    add search page

--- student_affairs/student_affairs_app/urls.py
+++ student_affairs/student_affairs_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('index', views.index, name='index'),
     path('view', views.view, name='view'),
-
+    path('search', views.search, name='search')
 ]
