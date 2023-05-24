310a75981cd77ddd43db6835c9c138ced52d7642
Fatma Elzahraa Ashraf <fatmaserry16@gmail.com>
2023-05-24 23:05:34 +0300

    add delete student from search

--- student_affairs/student_affairs_app/urls.py
+++ student_affairs/student_affairs_app/urls.py
@@ -4,7 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('index', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('view', views.view, name='view'),
-    path('search', views.search, name='search'),
-    path('edit_student', views.edit_student, name='edit_student')
+    path('search/', views.search, name='search'),
+    path('index/search/', views.search, name='search'),
+    path('view', views.search, name='view'),
+    path('<int:studentID>', views.edit_student, name='edit_student'),
+    path('delete-student/', views.delete_student, name='delete_student'),
+    # path('<int:studentID>', views.edit_department, name='edit_department')
+
 ]
